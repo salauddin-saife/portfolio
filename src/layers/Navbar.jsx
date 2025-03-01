3dec25c51ee436772e2234aba997162d1383a425
salauddin-saife <salauddin15saife@gmail.com>
2025-03-02 03:10:49 +0600

    done

--- src/layers/Navbar.jsx
+++ src/layers/Navbar.jsx
@@ -16,7 +16,7 @@ import Cart_3 from "../components/Cart_3";
 import Review2 from "../assets/Review2.png";
 import Review3 from "../assets/Review3.png";
 import Review4 from "../assets/Review4.png";
-
+import BG3 from "../assets/BG3.png";
 const Navbar = () => {
   return (
     <div>
@@ -173,7 +173,7 @@ const Navbar = () => {
       <section
         className="max-w-[1920px] pt-[200px] bg-center bg-cover bg-no-repeat mx-auto h-[2500px] bg-[#A793AC]"
         style={{
-          backgroundImage: `url(${BG2})`,
+          backgroundImage: `url(${BG3})`,
         }}
       >
         <Container>
@@ -209,7 +209,7 @@ const Navbar = () => {
       >
         <Container>
           <div className=" ">
-            <div className="font-Inter font-semibold text-[55px] text-main-text pt-[280px] text-center">
+            <div className="font-Inter font-semibold text-[55px] text-main-text pt-[80px] text-center">
               <h1>Customer Review</h1>
             </div>
             <div className="flex justify-between pt-[150px]">
@@ -224,7 +224,7 @@ const Navbar = () => {
            Our Best section
           ================================= */}
         <Container>
-          <div className="font-Inter font-semibold text-[55px] text-main-text pt-[100px] text-center">
+          <div className="font-Inter font-semibold text-[55px] text-main-text pt-[50px] text-center">
             <h1>Our Best o2</h1>
           </div>
           <div className="pt-[100px]">
@@ -233,21 +233,65 @@ const Navbar = () => {
         </Container>
 
         {/* =================================
-           Our Best section
+          Footer section
           ================================= */}
 
-       <div className="pt-[100px]">
-       <div
-          className=" w-[1920px] h-[571px]  bg-[#604E8A]/30 backdrop-blur-sm  "
-          style={{
-            boxShadow: "10px 13px 17px -3px rgba(96,78,138,0.75)",
-          }}
-        >
-          <Container>
-            <h1>Hwllo</h1>
-          </Container>
+        <div className="pt-[350px]">
+          <div
+            className=" w-[1920px] h-[540px]  bg-[#604E8A]/30 backdrop-blur-sm  "
+            style={{
+              boxShadow: "10px 13px 17px -3px rgba(96,78,138,0.75)",
+            }}
+          >
+            <Container>
+              <div className="flex justify-between pt-[100px]">
+                <div className="">
+                  <div className="flex mt-[-60px] w-[250px] h-[250px]">
+                    <Image src={My_Logo} />
+                  </div>
+                  <div className="w-[580px] h-[102px] font-Inter font-medium text-[28px] text-main-text">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </p>
+                  </div>
+                </div>
+                <div className="">
+                  <div className="font-Inter font-extrabold text-main-text text-[28px]">
+                    <h1>Quick Link’s</h1>
+                  </div>
+                  <div className=" font-Inter font-medium text-main-text text-[24px] ">
+                    <ul className="space-y-10 pt-[70px]">
+                      <Li li_text={"Home"} />
+                      <Li li_text={"Type’s Of plant’s"} />
+                      <Li li_text={"Contact"} />
+                      <Li li_text={"Privacy"} />
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                  <div className="font-Inter font-extrabold text-main-text text-[28px] pb-[40px]">
+                    <h1>For Every Update.</h1>
+                  </div>
+                  <div className="border-2 border-main-text  w-[562px] h-[74px] flex justify-between rounded-2xl">
+                    <input
+                      type="text"
+                      placeholder="Enter Email"
+                      className="w-full text-main-text font-Inter pl-[20px] font-medium text-[24px]"
+                    />
+                    <div className="inline-block ">
+                      <Button Button_text={"Subscribe"} />
+                    </div>
+                  </div>
+                  <div className=" text-main-text font-Inter font-medium text-[24px] pt-[250px]">
+                    <h1>planto © all right reserve</h1>
+                  </div>
+                </div>
+              </div>
+            </Container>
+          </div>
         </div>
-       </div>
       </section>
     </div>
   );

f35d1ba7959c037d70421e01122b4bd877cd5b27
salauddin-saife <salauddin15saife@gmail.com>
2025-03-01 22:04:50 +0600

    ok

--- src/layers/Navbar.jsx
+++ src/layers/Navbar.jsx
@@ -10,61 +10,67 @@ import { FaCirclePlay } from "react-icons/fa6";
 const Navbar = () => {
   return (
     <div>
-      <section
-        className="max-w-[1920px] mx-auto h-[1530px] bg-center bg-cover bg-no-repeat pt-[] bg-[#1B2316] mt-[]"
-        style={{ backgroundImage: `url(${BG1})`, backgroundPosition: "top -10px center"  }}
-      >
-        <Container>
-          <div className="flex">
-            <div className="w-4/12  h-[153px] ">
-              <div className="flex items-center justify- w-[150px] h-[150px] ">
-                <Image src={My_Logo} />
+      <section>
+        <div
+          className="max-w-[1920px] mx-auto h-[1530px] bg-center bg-cover bg-no-repeat pt-[] bg-[#1B2316] mt-[]"
+          style={{
+            backgroundImage: `url(${BG1})`,
+            backgroundPosition: " top -300px center",
+            backgroundSize: "",
+          }}
+        >
+          <Container>
+            <div className="flex">
+              <div className="w-4/12  h-[153px] ">
+                <div className="flex items-center justify- w-[150px] h-[150px] ">
+                  <Image src={My_Logo} />
+                </div>
+              </div>
+              <div className="w-8/12  h-[153px]  gap-x-8 flex justify-end items-center">
+                <div className="font-Inter text-main-text font-medium text-[24px]">
+                  <ul className="flex gap-x-10 ">
+                    <Li li_text={"Hello"} />
+                    <Li li_text={"About"} />
+                    <Li li_text={"Service"} />
+                    <Li li_text={"Portfolio"} />
+                    <Li li_text={"Testimonial"} />
+                    <Li li_text={"Skills"} />
+                  </ul>
+                </div>
+                <div>
+                  <Button Button_text={"Hello"} />
+                </div>
               </div>
             </div>
-            <div className="w-8/12  h-[153px]  gap-x-8 flex justify-end items-center">
-              <div className="font-Inter text-main-text font-medium text-[24px]">
-                <ul className="flex gap-x-10 ">
-                  <Li li_text={"Hello"} />
-                  <Li li_text={"About"} />
-                  <Li li_text={"Service"} />
-                  <Li li_text={"Portfolio"} />
-                  <Li li_text={"Testimonial"} />
-                  <Li li_text={"Skills"} />
-                </ul>
-              </div>
-              <div>
-                <Button Button_text={"Hello"} />
-              </div>
-            </div>
-          </div>
-        </Container>
+          </Container>
 
-        <Container container_class={"pt-[150px] max-w-[1920px]  mx-auto "}>
-          <div className="">
-            <div className="text-main-text text-[38px] font-normal font-Inter">
-              <h1>Hi, I am</h1>
-            </div>
-            <div className="text-main-text text-[118px] font-semibold font-Inter">
-              <h1>Salauddin Saife</h1>
-            </div>
-            <div className="text-main-text text-[23px] font-medium font-Inter w-[893px] ">
-              <h1>
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </h1>
-            </div>
-          </div>
-          <div className="flex items-center  gap-x-10 pt-[50px]">
-            <Button Button_text={"Explore"} />
-            <div className="flex items-center gap-4">
-              <FaCirclePlay className="text-[70px] text-main-text hover:text-main-text/70 duration-300  hover:scale-[110%]" />
-              <div className="text-main-text font-Inter font-normal text-[20px]">
-                <h1>Live Demo...</h1>
+          <Container container_class={"pt-[150px] max-w-[1920px]  mx-auto "}>
+            <div className="">
+              <div className="text-main-text text-[38px] font-normal font-Inter">
+                <h1>Hi, I am</h1>
+              </div>
+              <div className="text-main-text text-[118px] font-semibold font-Inter">
+                <h1>Salauddin Saife</h1>
+              </div>
+              <div className="text-main-text text-[23px] font-medium font-Inter w-[893px] ">
+                <h1>
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </h1>
               </div>
             </div>
-          </div>
-        </Container>
+            <div className="flex items-center  gap-x-10 pt-[50px]">
+              <Button Button_text={"Explore"} />
+              <div className="flex items-center gap-4">
+                <FaCirclePlay className="text-[70px] text-main-text hover:text-main-text/70 duration-300  hover:scale-[110%]" />
+                <div className="text-main-text font-Inter font-normal text-[20px]">
+                  <h1>Live Demo...</h1>
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
       </section>
     </div>
   );
